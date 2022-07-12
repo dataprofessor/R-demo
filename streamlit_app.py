8ee821030c7e01f5c3c4cefd7b4c73de2a439c41
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-07-12 14:40:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,15 @@ st.header('🎈 App Name')
 
 
 st.subheader('Printing text in R')
-
+code1 = '''print("Hello world ...")
+'''
+st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
 st.write(result1)
 
 
 st.subheader('Creating a plot using ggplot2')
-
 code2 = '''library(ggplot2)
 
 ggplot(mtcars, aes(mpg, wt)) +
@@ -22,7 +23,6 @@ ggplot(mtcars, aes(mpg, wt)) +
 ggsave('plot.png')
 '''
 st.code(code2, language='R')
-
 process2 = subprocess.Popen(["Rscript", "plot.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2 = process2.communicate()
 image = Image.open('plot.png')
