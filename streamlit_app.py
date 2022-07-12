9c46e9d07f81ff3afe42dd97647042e836484548
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-07-12 13:50:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ result2 = process.communicate()
 st.write(result2)
 
 
-process = subprocess.Popen(["R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+process = subprocess.Popen(["R CMD BATCH", "helloworld.r"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2 = process.communicate()
 st.write(result2)
