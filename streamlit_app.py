d0a9698bbf4dce690447928146433d18ac109e26
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-07-12 14:29:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,26 @@ from PIL import Image
 
 st.header('🎈 App Name')
 
-st.write('Hello world!')
 
-process1 = subprocess.Popen(["echo", "Hello, World!"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-result2 = process1.communicate()
-st.write(result2)
+st.subheader('Printing text in R')
 
-process2 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+result1 = process1.communicate()
+st.write(result1)
+
+
+st.subheader('Creating a plot using ggplot2')
+
+code2 = '''library(ggplot2)
+
+ggplot(mtcars, aes(mpg, wt)) +
+  geom_point()
+
+ggsave('plot.png')
+'''
+st.code(code2, language='R')
+
+process2 = subprocess.Popen(["Rscript", "plot.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result2 = process2.communicate()
-st.write(result2)
-
-process = subprocess.Popen(["Rscript", "plot.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-result3 = process.communicate()
 image = Image.open('plot.png')
 st.image(image)
