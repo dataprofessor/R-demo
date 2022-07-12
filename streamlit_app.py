8906c11be51530db0cfaeba17d421f362a249ca0
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-07-12 13:29:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import subprocess
 
 st.header('🎈 App Name')
 
 st.write('Hello world!')
+
+result1 = subprocess.run(["echo", "Hello, World!"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+st.write(result1.stdout)
+
+result2 = subprocess.run(["R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+st.write(result2.stdout)
